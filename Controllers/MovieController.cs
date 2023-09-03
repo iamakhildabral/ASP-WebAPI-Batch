e0953bf390bfb4f8b2e6908d887b24fb5691beaf
iamakhildabral <akhildabral02@gmail.com>
2023-09-03 09:30:22 +0530

    added multiple delete controller

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -72,7 +72,7 @@ namespace TestAPI.Controllers
 
         [HttpPut]
         [Route("/updateOne")]
-        public async Task<IActionResult> UpdateMovie([FromBody] Movie movie)
+        public IActionResult UpdateMovie([FromBody] Movie movie)
         {
             IActionResult result = CommonUpdateLogic(movie);
 
@@ -81,7 +81,7 @@ namespace TestAPI.Controllers
 
         [HttpPut]
         [Route("/updateMany")]
-        public async Task<IActionResult> updateMany([FromBody] List<Movie> movies)
+        public IActionResult updateMany([FromBody] List<Movie> movies)
         {
             if (movies == null || !movies.Any())
             {
@@ -132,14 +132,64 @@ namespace TestAPI.Controllers
         {
             if (movieName == null)
             {
-                return BadRequest(400);
+                return BadRequest("Movie name cannot be null.");
             }
             Movie? movie = _db.Movies.FirstOrDefault(movie => movie.Name == movieName);
             if (movie == null)
             {
                 return BadRequest("No such movie found");
             }
+            _db.Remove(movie);
+            _db.SaveChanges();
+            return Ok(movie);
+        }
 
+        [HttpDelete]
+        [Route("deleteOne")]
+        public IActionResult DeleteOne(string movieName)
+        {
+            IActionResult result = CommonDeleteLogic(movieName);
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("deleteMany")]
+        public IActionResult DeleteMany([FromBody] List<string> names)
+        {
+
+            if (names == null)
+            {
+                return BadRequest("Bad Content");
+            }
+            IActionResult result = Ok("All Records removed");
+
+            foreach (var movieName in names)
+            {
+                try
+                {
+                    IActionResult deleteResult = CommonDeleteLogic(movieName);
+                    if (deleteResult is ObjectResult objectResult && objectResult.StatusCode == 404)
+                    {
+                        return BadRequest("Movie Name not Found");
+
+
+                    }
+                }
+                catch
+                {
+                    return BadRequest("500, Internal Server Error");
+
+                }
+            }
+            if (result is null)
+            {
+                return BadRequest("No Such movies found");
+            }
+            else
+            {
+                return Ok("Data Deleted Successfully");
+
+            }
         }
     }
 }
